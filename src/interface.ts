f0069b56e7df9f33522f13f2bd45669d8a8b7496
SerhiiChynin <sergey.chinin@gmail.com>
2023-08-22 11:43:15 +0300

    interface

--- src/interface.ts
+++ src/interface.ts
@@ -17,34 +17,34 @@ interface Repository<T> {
 
 // const rep = new Repository();
 
-class UserRepository implements Repository<User> {
-    create(obj: User): User { 
-        // return database.query(INSERT ...)
-    }
-    get(): User {
-        return 'Serhii';
-    }
-    delete(obj: User): User{
-        return undefined;
+// class UserRepository implements Repository<User> {
+//     create(obj: User): User { 
+//         // return database.query(INSERT ...)
+//     }
+//     get(): User {
+//         return {username: 'Serhii', age: 22};
+//     }
+//     delete(obj: User): User{
+//         return undefined;
 
-    }
-    update(obj: User): User{
-        return undefined;
-    }
-}
+//     }
+//     update(obj: User): User{
+//         return undefined;
+//     }
+// }
 
-class CarRepository implements Repository<Cars> {
-    create(obj: Cars): Cars { 
-        // return database.query(INSERT ...)
-    }
-    get(): Cars {
-        return 'Serhii';
-    }
-    delete(obj: Cars): Cars{
-        return undefined;
+// class CarRepository implements Repository<Cars> {
+//     create(obj: Cars): Cars { 
+//         // return database.query(INSERT ...)
+//     }
+//     get(): Cars {
+//         return 'Serhii';
+//     }
+//     delete(obj: Cars): Cars{
+//         return undefined;
 
-    }
-    update(obj: Cars): Cars{
-        return undefined;
-    }
-}
+//     }
+//     update(obj: Cars): Cars{
+//         return undefined;
+//     }
+// }
